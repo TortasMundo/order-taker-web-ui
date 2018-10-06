42090c72d61f7342014ffd585217afde751ee60a
alayor <alayor3@gmail.com>
2018-10-06 15:38:11 -0600

    Call place service.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,80 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import './tacit-css.min.css'
 import './App.css'
+import * as orderService from './services/orders'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.initialState = {
+      jamon: '0',
+      lomo: '0',
+      especial: '0',
+      refrescos: '0',
+      notes: '',
+    }
+    this.state = this.initialState
+  }
+
+  changeOrder = item => event => {
+    this.setState({[item]: event.target.value})
+  }
+
+  place = async () => {
+    const result = await orderService.placeOrder(this.jamon, this.lomo, this.especial, this.refrescos, this.notes)
+    if (result.success) {
+      this.state = this.initialState
+    }
+  }
+
   render() {
     return (
       <div>
         <form>
           <div>
-          Jamón{' '}
-          <select>
-            {
-              Array(21).fill().map((_, i) => <option>{i}</option>)
-            }
-          </select>
+            Jamón{' '}
+            <select onChange={this.changeOrder('jamon')}>
+              {Array(21)
+                .fill()
+                .map((_, i) => (
+                  <option key={i}>{i}</option>
+                ))}
+            </select>
           </div>
           <div>
             Lomo{' '}
-            <select>
-              {
-                Array(21).fill().map((_, i) => <option>{i}</option>)
-              }
+            <select onChange={this.changeOrder('lomo')}>
+              {Array(21)
+                .fill()
+                .map((_, i) => (
+                  <option key={i}>{i}</option>
+                ))}
             </select>
           </div>
           <div>
             Especial{' '}
-            <select>
-              {
-                Array(21).fill().map((_, i) => <option>{i}</option>)
-              }
+            <select onChange={this.changeOrder('especial')}>
+              {Array(21)
+                .fill()
+                .map((_, i) => (
+                  <option key={i}>{i}</option>
+                ))}
             </select>
           </div>
           <div>
             Refrescos{' '}
-            <select>
-              {
-                Array(21).fill().map((_, i) => <option>{i}</option>)
-              }
+            <select onChange={this.changeOrder('refrescos')}>
+              {Array(21)
+                .fill()
+                .map((_, i) => (
+                  <option key={i}>{i}</option>
+                ))}
             </select>
           </div>
           <div>
-            Notas {' '}
-            <input className='notes' type='text' />
+            Notas <input className="notes" type="text" onChange={this.changeOrder('notes')}/>
           </div>
-          <input type="submit" value="Enviar" />
+          <input type="submit" value="Enviar" onClick={this.place}/>
         </form>
       </div>
     )
