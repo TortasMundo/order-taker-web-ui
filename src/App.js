9221540dd1ff4105c0610370eb9ddbb5d5d987e8
Alonso A. Ortega <alayor3@gmail.com>
2018-10-26 21:18:03 -0600

    Move code to Form component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react'
 import './tacit-css.min.css'
 import * as orderService from './services/orders'
-import { Form } from './ui/Form'
+import Form from './ui/Form'
 import { Table } from './ui/Table'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.initialState = {
-      jamon: '0',
-      lomo: '0',
-      especial: '0',
-      refrescos: '0',
-      notes: '',
-      idle: true,
-    }
+
     this.state = {
-      ...this.initialState,
       orders: []
     }
   }
@@ -33,35 +25,10 @@ class App extends Component {
     }
   }
 
-  changeQuantity = item => event => {
-    this.setState({ [item]: event.target.value })
-  }
-
-  place = async () => {
-    this.setState({ idle: false })
-    const result = await orderService.placeOrder(
-      this.state.jamon,
-      this.state.lomo,
-      this.state.especial,
-      this.state.refrescos,
-      this.state.notes,
-    )
-    await this.getOrders()
-    this.setState({ idle: true })
-    if (result.success) {
-      this.setState({
-        ...this.initialState,
-      })
-      alert('La orden fue creada correctamente.')
-    } else {
-      alert(result.error)
-    }
-  }
-
   render() {
     return (
       <div>
-        <Form changeQuantity={this.changeQuantity} place={this.place} {...this.state} />
+        <Form getOrders={this.getOrders}/>
         <Table orders={this.state.orders}/>
       </div>
     )
--- src/ui/Form/index.js
+++ src/ui/Form/index.js
@@ -1,54 +1,102 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { styles } from './styles'
 import Radium from 'radium'
+import App from '../../App'
+import * as orderService from '../../services/orders'
 
-export const Form = (props) => {
-  return ( <div style={styles.container}>
-    <form>
-      <div>
-        Jamón{' '}
-        <select onChange={props.changeQuantity('jamon')} value={props.jamon}>
-          {Array(21)
-            .fill()
-            .map((_, i) => (
-              <option key={i}>{i}</option>
-            ))}
-        </select>
-      </div>
-      <div>
-        Lomo{' '}
-        <select onChange={props.changeQuantity('lomo')} value={props.lomo}>
-          {Array(21)
-            .fill()
-            .map((_, i) => (
-              <option key={i}>{i}</option>
-            ))}
-        </select>
-      </div>
-      <div>
-        Especial{' '}
-        <select onChange={props.changeQuantity('especial')} value={props.especial}>
-          {Array(21)
-            .fill()
-            .map((_, i) => (
-              <option key={i}>{i}</option>
-            ))}
-        </select>
-      </div>
-      <div>
-        Refrescos{' '}
-        <select onChange={props.changeQuantity('refrescos')} value={props.refrescos}>
-          {Array(21)
-            .fill()
-            .map((_, i) => (
-              <option key={i}>{i}</option>
-            ))}
-        </select>
-      </div>
-      <div>
-        Notas <input style={styles.notes} type="text" onChange={props.changeQuantity('notes')} value={props.notes}/>
-      </div>
-      <button style={styles.button} type="button" onClick={props.place} disabled={props.idle === false}>Enviar</button>
-    </form>
-  </div>)
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.initialState = {
+      jamon: '0',
+      lomo: '0',
+      especial: '0',
+      refrescos: '0',
+      notes: '',
+      idle: true,
+    }
+    this.state = {
+      ...this.initialState,
+      orders: []
+    }
+  }
+
+  place = async () => {
+    this.setState({ idle: false })
+    const result = await orderService.placeOrder(
+      this.state.jamon,
+      this.state.lomo,
+      this.state.especial,
+      this.state.refrescos,
+      this.state.notes,
+    )
+    await this.props.getOrders()
+    this.setState({ idle: true })
+    if (result.success) {
+      this.setState({
+        ...this.initialState,
+      })
+      alert('La orden fue creada correctamente.')
+    } else {
+      alert(result.error)
+    }
+  }
+
+  changeQuantity = item => event => {
+    this.setState({ [item]: event.target.value })
+  }
+
+  render() {
+    return (<div style={styles.container}>
+      <form>
+        <div>
+          Jamón{' '}
+          <select onChange={this.changeQuantity('jamon')} value={this.state.jamon}>
+            {Array(21)
+              .fill()
+              .map((_, i) => (
+                <option key={i}>{i}</option>
+              ))}
+          </select>
+        </div>
+        <div>
+          Lomo{' '}
+          <select onChange={this.changeQuantity('lomo')} value={this.state.lomo}>
+            {Array(21)
+              .fill()
+              .map((_, i) => (
+                <option key={i}>{i}</option>
+              ))}
+          </select>
+        </div>
+        <div>
+          Especial{' '}
+          <select onChange={this.changeQuantity('especial')} value={this.state.especial}>
+            {Array(21)
+              .fill()
+              .map((_, i) => (
+                <option key={i}>{i}</option>
+              ))}
+          </select>
+        </div>
+        <div>
+          Refrescos{' '}
+          <select onChange={this.changeQuantity('refrescos')} value={this.state.refrescos}>
+            {Array(21)
+              .fill()
+              .map((_, i) => (
+                <option key={i}>{i}</option>
+              ))}
+          </select>
+        </div>
+        <div>
+          Notas <input style={styles.notes} type="text" onChange={this.changeQuantity('notes')} value={this.state.notes}/>
+        </div>
+        <button style={styles.button} type="button" onClick={this.place} disabled={this.state.idle === false}>Enviar
+        </button>
+      </form>
+    </div>)
+  }
 }
+
+export default Form
