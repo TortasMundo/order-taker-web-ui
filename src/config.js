8505bbc65b765963f06c2db81c870932c6301528
Alonso A. Ortega <alayor3@gmail.com>
2018-11-03 21:27:40 -0700

    Point to aws server api.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 module.exports = {
   get API_URL() {
-    return window.location.href === 'https://tortasmundo-order-taker-web-ui.herokuapp.com/'
-      ? `https://tortasmundo-order-taker-api.herokuapp.com` : 'http://localhost:4001'
+    if (window.location.href === 'https://tortasmundo-order-taker-web-ui.herokuapp.com/') {
+      return `https://tortasmundo-order-taker-api.herokuapp.com`
+    }
+    if (window.location.href === 'http://tortasmundo-order-taker-web-ui-1588083847.us-east-2.elb.amazonaws.com/') {
+      return 'http://tortasmundo-order-taker-api-560142853.us-east-2.elb.amazonaws.com'
+    }
+    return  'http://localhost:4001'
   },
   mocks: {
     store_location: {
