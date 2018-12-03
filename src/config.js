acef34f147dcf01879d2187cf9d758b85f51e721
Alonso A. Ortega <alayor3@gmail.com>
2018-12-03 12:14:07 -0700

    Modify api port

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ module.exports = {
       return 'http://tortasmundo-order-taker-api-560142853.us-east-2.elb.amazonaws.com'
     }
     if (window.location.href === 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:3001/') {
-      return 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:4001'
+      return 'http://ec2-18-218-154-49.us-east-2.compute.amazonaws.com:5001'
     }
     return  'http://localhost:4001'
   },
